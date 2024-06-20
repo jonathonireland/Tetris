ad57196ae31c7ca9e17c4abaaa0d12dc837a49f8
Jonathon Ireland <59446203+jonathonireland@users.noreply.github.com>
2024-06-20 01:38:31 -0400

    Finished Position Class @8:59

---
+++ Position.cs
@@ -0,0 +1,13 @@
+﻿namespace Tetris
+{
+    class Position
+    {
+        public int Row { get; set; }
+        public int Column { get; set; }
+        public Position(int row, int column)
+        {
+            Row = row;
+            Column = column;
+        }
+    }
+}
