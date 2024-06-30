481b93ca5d742488225a7d794e083d69fa2be943
Jonathon Ireland <59446203+jonathonireland@users.noreply.github.com>
2024-06-30 16:24:12 -0400

    working tetris game

--- GameState.cs
+++ GameState.cs
@@ -8,23 +8,35 @@
         {
             get => currentBlock;
             private set
-            { 
+            {
                 currentBlock = value;
                 currentBlock.Reset();
+
+                for (int i = 0; i < 2; i++)
+                {
+                    currentBlock.Move(1, 0);
+
+                    if (!BlockFits())
+                    {
+                        currentBlock.Move(-1, 0);
+                    }
+                }
             }
         }
 
         public GameGrid GameGrid { get; }
-
         public BlockQueue BlockQueue { get; }
-
         public bool GameOver { get; private set; }
+        public int Score { get; private set; }
+        public Block HeldBlock { get; private set; }
+        public bool CanHold { get; private set; }
 
         public GameState()
         {
             GameGrid = new GameGrid(22, 10);
             BlockQueue = new BlockQueue();
             CurrentBlock = BlockQueue.GetAndUpdate();
+            CanHold = true;
         }
 
         private bool BlockFits()
@@ -36,14 +48,37 @@
                     return false;
                 }
             }
+
             return true;
+        }
+
+        public void HoldBlock()
+        {
+            if (!CanHold)
+            {
+                return;
+            }
+
+            if (HeldBlock == null)
+            {
+                HeldBlock = CurrentBlock;
+                CurrentBlock = BlockQueue.GetAndUpdate();
+            }
+            else
+            {
+                Block tmp = CurrentBlock;
+                CurrentBlock = HeldBlock;
+                HeldBlock = tmp;
+            }
+
+            CanHold = false;
         }
 
         public void RotateBlockCW()
         {
             CurrentBlock.RotateCW();
 
-            if(!BlockFits())
+            if (!BlockFits())
             {
                 CurrentBlock.RotateCCW();
             }
@@ -53,19 +88,19 @@
         {
             CurrentBlock.RotateCCW();
 
-            if (!BlockFits()) 
+            if (!BlockFits())
             {
                 CurrentBlock.RotateCW();
             }
         }
 
-        public void MoveBlockLeft() 
+        public void MoveBlockLeft()
         {
             CurrentBlock.Move(0, -1);
 
-            if(!BlockFits())
+            if (!BlockFits())
             {
-                CurrentBlock.Move(0, -1);
+                CurrentBlock.Move(0, 1);
             }
         }
 
@@ -73,7 +108,7 @@
         {
             CurrentBlock.Move(0, 1);
 
-            if (!BlockFits()) 
+            if (!BlockFits())
             {
                 CurrentBlock.Move(0, -1);
             }
@@ -84,22 +119,23 @@
             return !(GameGrid.IsRowEmpty(0) && GameGrid.IsRowEmpty(1));
         }
 
-        private void PlaceBlock() 
+        private void PlaceBlock()
         {
             foreach (Position p in CurrentBlock.TilePositions())
             {
                 GameGrid[p.Row, p.Column] = CurrentBlock.Id;
             }
 
-            GameGrid.ClearFullRows();
+            Score += GameGrid.ClearFullRows();
 
-            if (IsGameOver()) 
+            if (IsGameOver())
             {
                 GameOver = true;
             }
             else
             {
                 CurrentBlock = BlockQueue.GetAndUpdate();
+                CanHold = true;
             }
         }
 
@@ -107,11 +143,41 @@
         {
             CurrentBlock.Move(1, 0);
 
-            if(!BlockFits())
+            if (!BlockFits())
             {
                 CurrentBlock.Move(-1, 0);
                 PlaceBlock();
             }
+        }
+
+        private int TileDropDistance(Position p)
+        {
+            int drop = 0;
+
+            while (GameGrid.IsEmpty(p.Row + drop + 1, p.Column))
+            {
+                drop++;
+            }
+
+            return drop;
+        }
+
+        public int BlockDropDistance()
+        {
+            int drop = GameGrid.Rows;
+
+            foreach (Position p in CurrentBlock.TilePositions())
+            {
+                drop = System.Math.Min(drop, TileDropDistance(p));
+            }
+
+            return drop;
+        }
+
+        public void DropBlock()
+        {
+            CurrentBlock.Move(BlockDropDistance(), 0);
+            PlaceBlock();
         }
     }
 }
